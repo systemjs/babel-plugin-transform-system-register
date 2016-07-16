5ae1f1b8971988a6110f89270e06c03ed43f9e59
Aliaksei Sapach <dreamsonic@tut.by>
2016-07-17 02:22:23 +0300

    set module name

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 export default function ({ types: t }) {
   return {
     visitor: {
-      MemberExpression(path, { opts = { systemGlobal: "System" } }) {
-        if (t.isIdentifier(path.node.object, { name: "System" }) &&
-            t.isIdentifier(path.node.property, { name: "register" })) {
-          path.node.object.name = opts.systemGlobal;
+      CallExpression(path, { opts = {} }) {
+        var callee = path.node.callee;
+        var args = path.node.arguments;
+        if (t.isMemberExpression(callee) &&
+            t.isIdentifier(callee.object, { name: 'System' }) &&
+            t.isIdentifier(callee.property, { name: 'register' })) {
+          callee.object.name = opts.systemGlobal || 'System';
+
+          // System.register(deps, declare)
+          if (opts.moduleName && t.isArrayExpression(args[0])) {
+            args.unshift(t.stringLiteral(opts.moduleName));
+          }
         }
       }
     }
