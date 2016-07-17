bb6b664411263e62e348c4764f96c500cc85a9a1
Aliaksei Sapach <dreamsonic@tut.by>
2016-07-17 16:13:40 +0300

    refactored test runner to accept options

--- test/index.js
+++ test/index.js
@@ -2,6 +2,8 @@ import path from 'path';
 import fs from 'fs';
 import assert from 'assert';
 import { transformFileSync } from 'babel-core';
+import fileExists from 'file-exists';
+import plugin from '../src';
 
 function trim(str) {
   return str.replace(/^\s+|\s+$/, '');
@@ -13,17 +15,25 @@ describe('Plugin', () => {
     it(`should ${caseName.split('-').join(' ')}`, () => {
       const fixtureDir = path.join(fixturesDir, caseName);
       const actualPath = path.join(fixtureDir, 'actual.js');
+      const optionsPath = path.join(fixtureDir, 'options.js');
+      const exceptionPath = path.join(fixtureDir, 'exception.txt');
+      const options = {
+        babelrc: false,
+        plugins: [plugin]
+      };
 
-      if (caseName.indexOf('reject') === 0) {
-        const exception = fs.readFileSync(
-            path.join(fixtureDir, 'exception.txt')
-        ).toString();
+      if (fileExists(optionsPath)) {
+        options.plugins = [[plugin, require(optionsPath)]];
+      }
+
+      if (fileExists(exceptionPath)) {
+        const exception = fs.readFileSync(exceptionPath).toString();
 
         assert.throws(() => {
-          transformFileSync(actualPath);
+          transformFileSync(actualPath, options);
         }, exception);
       } else {
-        const actual = transformFileSync(actualPath).code;
+        const actual = transformFileSync(actualPath, options).code;
         const expected = fs.readFileSync(
             path.join(fixtureDir, 'expected.js')
         ).toString();
