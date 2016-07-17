b6d2574dd5a687d0508e22e022bfc1af80678e6c
Aliaksei Sapach <dreamsonic@tut.by>
2016-07-17 16:41:09 +0300

    normalize dependencies

--- src/index.js
+++ src/index.js
@@ -9,12 +9,24 @@ export default function ({ types: t }) {
             t.isIdentifier(callee.property, { name: 'register' })) {
           callee.object.name = opts.systemGlobal || 'System';
 
+          const firstArg = args[0];
+
           // System.register(deps, declare)
-          if (t.isArrayExpression(args[0])) {
+          if (t.isArrayExpression(firstArg)) {
             if (this.hasAnonRegister) {
-              throw new Error('Source ' + this.name + ' has multiple anonymous System.register calls.');
+              throw new Error(`Source ${this.name} has multiple anonymous System.register calls.`);
             }
+
+            // normalize dependencies in array
+            // NB add metadata.deps here too
+            if (typeof opts.map === 'function') {
+              firstArg.elements.forEach(e => {
+                e.value = opts.map(e.value);
+              });
+            }
+
             this.hasAnonRegister = true;
+
             if (opts.moduleName) {
               args.unshift(t.stringLiteral(opts.moduleName));
             }
